af5bbc4a33da1657403fe12cb97b04c327acad85
arafatah <arafathowlader890@gmail.com>
2024-10-26 13:05:09 +0600

    select, create, insert element in dom

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ console.log(document.documentElement); // The <html> element
 console.log(document.head); // The <head> element
 console.log(document.body); // The <body> element
 
-document.querySelector('.header'); // The <header> element
+const header = document.querySelector('.header'); // The <header> element
 const allSections = document.querySelectorAll('.section'); // NodeList(4) [section#section--1, section#section--2, section#section--3, section#section--4]
 console.log(allSections);
 
@@ -46,5 +46,24 @@ console.log(allButtons);
 //creating and inserting elements
 // document.querySelector('.header').insertAdjacentHTML('afterend', '<h2>Test</h2>');
 
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
+// header.insertAdjacentElement('beforeend', message); // insert after the last child
+// header.prepend(message); // insert before the first child
+// header.append(message.cloneNode(true)); // insert multiple copies of the same element
+header.append(message); // insert after the last child
 
+//header.before(message); // insert before the first child
+//header.after(message); // insert after the last child
+
+//delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
