1e3d7f721d4bb09da8e26f7474ee71f63bb59b44
arafatah <arafathowlader890@gmail.com>
2024-10-27 12:24:30 +0600

    event propagation practice

--- script.js
+++ script.js
@@ -142,9 +142,34 @@ const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
   h1.removeEventListener('mouseenter', alertH1);
 };
+
 h1.addEventListener('mouseenter', alertH1);
 
 //Old school way
 // h1.onmouseenter = function (e) {
 //   alert('onmouseEnter: Great! You are reading the heading :D');
 // };
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+//rag(255, 255, 255)
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+});
