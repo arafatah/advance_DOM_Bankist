66b3f7ac7bf341d51ea4eed46a10d57f85f00cbe
arafatah <arafathowlader890@gmail.com>
2024-10-29 15:48:35 +0600

    observer add, reveal section function created

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ const obsOptions = {
 const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
  */
-const header = document.querySelector('.header');
+/* const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 // console.log(navHeight);
 
@@ -159,9 +159,50 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-{navHeight}px`,
+  rootMargin: `-${navHeight}px`,
+});
+// console.log(headerObserver);
+headerObserver.observe(header);
+ */
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserve = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserve.observe(section);
+  section.classList.add('section--hidden');
+});
 
 /*console.log(document.documentElement); // The <html> element
 console.log(document.head); // The <head> element
